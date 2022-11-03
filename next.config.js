40fca5652cad0eac1a40b320a257ed37fdaabdf6
igorskiter <igor.nw3@gmail.com>
2022-11-03 12:25:23 -0300

    feat: adding sentry

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+const { withSentryConfig } = require('@sentry/nextjs');
 const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development'
@@ -11,4 +12,17 @@ const nextConfig = {
   },
 }
 
-module.exports = withPWA(nextConfig);
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  hideSourceMaps: false,
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+module.exports = withSentryConfig(withPWA(nextConfig), sentryWebpackPluginOptions);
