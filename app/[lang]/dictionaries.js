0694c4d43ac1b5520654e0afdb9182d2e1c66672
igorskiter <igor.nw3@gmail.com>
2023-12-21 14:42:19 -0300

    feat: adjusting list dictionarie

--- app/[lang]/dictionaries.js
+++ app/[lang]/dictionaries.js
@@ -3,8 +3,8 @@
 import 'server-only';
 
 const dictionaries = {
-  br: () => import('../../dictionaries/br.json').then((module) => module.default),
   en: () => import('../../dictionaries/en.json').then((module) => module.default),
+  br: () => import('../../dictionaries/br.json').then((module) => module.default),
 };
 
 export const getDictionary = async (locale) => dictionaries[locale]();
